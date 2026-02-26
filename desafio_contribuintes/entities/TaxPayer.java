c577e350b35eaa3dec6fe45061519396548cebb1
patricpassos <patrickpassos93@gmail.com>
2026-02-26 19:49:55 -0300

    final version adjustments

--- desafio_contribuintes/entities/TaxPayer.java
+++ desafio_contribuintes/entities/TaxPayer.java
@@ -1,14 +1,15 @@
 package entities;
 
 public class TaxPayer {
-	
+
 	private double salaryIncome;
 	private double servicesIncome;
 	private double capitalIncome;
 	private double healthSpending;
 	private double educationSpending;
-	
-	public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending) {
+
+	public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending,
+			double educationSpending) {
 		this.salaryIncome = salaryIncome;
 		this.servicesIncome = servicesIncome;
 		this.capitalIncome = capitalIncome;
@@ -55,57 +56,55 @@ public class TaxPayer {
 	public void setEducationSpending(double educationSpending) {
 		this.educationSpending = educationSpending;
 	}
-	
+
 	private double monthlySalary() {
-	    return salaryIncome / 12.0;
+		return salaryIncome / 12.0;
 	}
-	
+
 	public boolean salaryTaxExempt() {
 		return monthlySalary() < 3000.0;
 	}
-	
+
 	public boolean salaryTax10() {
 		return monthlySalary() < 5000.0;
 	}
-	
+
 	public double salaryTax() {
-		if(salaryTaxExempt()) {
+		if (salaryTaxExempt()) {
 			return 0.0;
-		} else if(salaryTax10()) {
+		} else if (salaryTax10()) {
 			return salaryIncome * 0.10;
 		} else {
 			return salaryIncome * 0.20;
 		}
 	}
-	
+
 	public double servicesTax() {
 		return servicesIncome * 0.15;
 	}
-	
+
 	public double capitalTax() {
 		return capitalIncome * 0.20;
 	}
-	
+
 	public double grossTax() {
 		return salaryTax() + servicesTax() + capitalTax();
 	}
-	
+
 	public double taxRebate() {
 		double deductibleExpenses = healthSpending + educationSpending;
 		double deductibleTax = grossTax() * 0.30;
 		return deductibleExpenses > deductibleTax ? deductibleTax : deductibleExpenses;
 	}
-	
+
 	public double netTax() {
 		return grossTax() - taxRebate();
 	}
-	
+
 	@Override
 	public String toString() {
-		return
-				"\nTotal gross tax: " + String.format("%.2f", grossTax())
-				+ "\nTax abatement: " + String.format("%.2f", taxRebate())
-				+ "\nTax due: " + String.format("%.2f", netTax());
+		return "\nTotal gross tax: " + String.format("%.2f", grossTax()) + "\nTax abatement: "
+				+ String.format("%.2f", taxRebate()) + "\nTax due: " + String.format("%.2f", netTax());
 	}
-	
+
 }
